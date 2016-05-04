20fb8fd269a72bedd48e108408b8414ec6564691
Quentin Delcourt <kant312@gmail.com>
2016-05-04 23:32:41 +0200

    Add script to allow galleries to be browsed more easily

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    var qs = document.querySelector.bind(document),
+        qsa = document.querySelectorAll.bind(document);
+
+    baguetteBox.run('[data-gallery]');
+
+})();
